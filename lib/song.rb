c0c01e63fe5245300509b8cac0ac557d8318ba02
Shami Tomita <shamitomita@gmail.com>
2021-01-24 21:42:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   end 
   
   def self.new_from_filename(filename)
-    filename.splice(".mp3")
+    filename.slice(".mp3")
     rows = filename.split(" - ")
     songs = rows.collect do |row|
     data = row.split(", ")
