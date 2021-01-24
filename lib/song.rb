5f2c4c7caa71abac2a74436c186af9a6118da7fd
Shami Tomita <shamitomita@gmail.com>
2021-01-24 20:52:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.find_or_create_by_name(name) 
     if name == self.find_by_name(name)
-      name 
+      song 
     else 
       song = self.new 
       song.name = name 
